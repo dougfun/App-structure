330884fb6af0176894f200a1b223ed1fb8926fd8
Douglas <doug.ie@outlook.ie>
2024-02-19 13:49:31 -0300

    9.Commit: Initial Navbar structure

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { Menu } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Sidebar from "@/components/ui/sidebar";
+
+const MobileSidebar = () => {
+    return (
+        <Sheet>
+            <SheetTrigger>
+                <Button variant="ghost" size="icon" className="md:hidden">
+                    <Menu />
+                </Button>
+            </SheetTrigger>
+            <SheetContent side="left" className="p-0">
+                <Sidebar />
+            </SheetContent>
+        </Sheet>
+    );
+}
+
+export default MobileSidebar;
+
